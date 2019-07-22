3b4cbf308a6097666c6f1f067deda88165daeea5
Travis Exline <t.exline22@gmail.com>
2019-07-22 19:30:06 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,10 +1,10 @@
 class SchoolClassesController < ApplicationController
-  def new
-    @school_class =SchoolClass.new
-  end
-
   def show
     @school_class = SchoolClass.find(params[:id])
+  end
+
+  def new
+    @school_class = SchoolClass.new
   end
 
   def edit
