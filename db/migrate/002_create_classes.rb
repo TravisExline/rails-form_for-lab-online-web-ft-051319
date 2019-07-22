dee2c105d9140ebffa160ec6e4bc709e60d36bec
Travis Exline <t.exline22@gmail.com>
2019-07-22 18:47:46 +0000

    Automatically backed up by Learn

---
+++ db/migrate/002_create_classes.rb
@@ -0,0 +1,10 @@
+class CreateClasses < ActiveRecord::Migration
+  def change
+    create_table :classes do |t|
+      t.string :title
+      t.integer :room_number
+
+      t.timestamps null: false
+    end
+  end
+end
