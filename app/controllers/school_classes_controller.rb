f5c1b01dffeeec936c2ecc7230089ef49f26f937
Travis Exline <t.exline22@gmail.com>
2019-07-22 19:23:21 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -24,4 +24,10 @@ class SchoolClassesController < ApplicationController
     @school_class.update()
     redirect_to school_class_path(@school_class)
   end
+
+  private
+
+  def school_class_params
+    params.require(:school_class).permit(:title, :room_number)
+  end
 end
