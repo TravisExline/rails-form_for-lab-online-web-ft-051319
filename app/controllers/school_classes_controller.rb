e90072ec09f509559355046eb9d88b4e3efa2cac
Travis Exline <t.exline22@gmail.com>
2019-07-22 19:18:30 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -18,5 +18,8 @@ class SchoolClassesController < ApplicationController
   end
 
   def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update()
+    redirect_to school_class_path(@school_class)
   end
 end
