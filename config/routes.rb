95d305b4f55318d433d49a12c73238b904a2566d
Travis Exline <t.exline22@gmail.com>
2019-07-22 19:00:32 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
- resources school_classes only: [:new, :create, :edit, :show, :update]
- resources students only: [:new, :create, :edit, :show, :update][]
+ resources :school_classes, only: [:new, :create, :edit, :show, :update]
+ resources :students, only: [:new, :create, :edit, :show, :update]
 end
